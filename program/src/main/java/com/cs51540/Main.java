a23591ef77bd7a760bf0a3f8033c187c96d1fd4d
Phiboy788 <hkroll788@gmail.com>
2024-04-23 19:54:34 -0500

    Changed IDatarepository to Datarepository everywhere errors arose

--- program/src/main/java/com/cs51540/Main.java
+++ program/src/main/java/com/cs51540/Main.java
@@ -1,11 +1,11 @@
 package com.cs51540; 
 import com.cs51540.data.TestDataRepository;
-import com.cs51540.interfaces.IDataRepository;
+import com.cs51540.data.DataRepository;
 import com.cs51540.views.Scheduler;
 
 public class Main{
     public static void main(String[] args) {
-    IDataRepository DataRepository = new TestDataRepository();  
+    DataRepository DataRepository = new DataRepository();  
 		Scheduler frame = new Scheduler(DataRepository);
 		frame.setVisible(true);
     }
